0c7ecdb0f35edd9638a7e69bd4219785a9532d37
Hodgy2448 <78037871+Hodgy2448@users.noreply.github.com>
2024-03-09 20:10:51 +0000

    Exercise 1 & 2

--- UserManagement.Web/Controllers/UsersController.cs
+++ UserManagement.Web/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using UserManagement.Models;
 using UserManagement.Services.Domain.Interfaces;
 using UserManagement.Web.Models.Users;
 
@@ -11,22 +12,50 @@ public class UsersController : Controller
     public UsersController(IUserService userService) => _userService = userService;
 
     [HttpGet]
-    public ViewResult List()
+    public ViewResult List(bool filter, bool filterByActive)
     {
-        var items = _userService.GetAll().Select(p => new UserListItemViewModel
+
+        IEnumerable<User> users;
+        
+        if (filter)
+        {
+            users = _userService.FilterByActive(filterByActive);
+        }
+        else
+        {
+            users = _userService.GetAll();
+        }
+
+        var items = users.Select(p => new UserListItemViewModel
         {
             Id = p.Id,
             Forename = p.Forename,
             Surname = p.Surname,
             Email = p.Email,
-            IsActive = p.IsActive
+            IsActive = p.IsActive,
+            DateOfBirth = p.DateOfBirth
         });
 
         var model = new UserListViewModel
         {
+            FilterByActive = filterByActive,
             Items = items.ToList()
         };
 
         return View(model);
-    }
+
+    // OR filter in controller below
+
+    // if(filterByActive){
+    //     items = items.Where(p => p.IsActive);
+    // }
+
+    // var model = new UserListViewModel
+    // {
+    //     FilterByActive = filterByActive,
+    //     Items = items.ToList()
+    // };
+
+    // return View(model);
+}
 }
